28d6cdd910fffca088a90133c012eb6d6619ffc9
esther0213 <138899462+Estherkim0213@users.noreply.github.com>
2023-09-08 12:55:53 -0500

    tried to add variables and methods

--- app/src/main/java/com/example/calculatorappkim/MainActivity.java
+++ app/src/main/java/com/example/calculatorappkim/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,43 +19,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    TextView resultTv,solutionTv;
+    MaterialButton buttonC, buttonMultiply,buttonBrackOpen,buttonBrackClose
+    , buttonDivide, buttonAdd,buttonMinus,buttonExponent,buttonRoot,buttonEquals, buttonDecimal;
+    MaterialButton button0,button1,button2,button3,button4,button5,button6,
+            button7,button8,button9;
+    private double numInput;
+    private ArrayList equation;
+
     public void numSelected(View v){
-        double numInput = 0;
         if (v.getId() == R.id.button1){
             Log.i("Esther","Selected1");
-            numInput = 1;
+            equation.add(1);
         }
         else if (v.getId() == R.id.button2){
             Log.i("Esther","Selected2");
-            numInput = 2;
+            equation.add(2);
         }
         else if (v.getId() == R.id.button3){
             Log.i("Esther","Selected3");
-            numInput = 3;
+            equation.add(3);
         }
         else if (v.getId() == R.id.button4){
             Log.i("Esther","Selected4");
-            numInput = 4;
+            equation.add(4);
         }
         else if (v.getId() == R.id.button5){
             Log.i("Esther","Selected5");
-            numInput = 5;
+            equation.add(5);
         }
         else if (v.getId() == R.id.button6){
             Log.i("Esther","Selected6");
-            numInput = 6;
+            equation.add(6);
         }
         else if (v.getId() == R.id.button7){
             Log.i("Esther","Selected7");
-            numInput = 7;
+            equation.add(7);
         }
         else if (v.getId() == R.id.button8){
             Log.i("Esther","Selected8");
-            numInput = 8;
+            equation.add(8);
         }
         else if (v.getId() == R.id.button9){
             Log.i("Esther","Selected9");
-            numInput = 9;
+            equation.add(9);
         }
+    }
+    public void operatorSelected(View v){
+        if (v.getId() == R.id.multiplyButton){
+            Log.i("Esther","SelectedMultiply");
+            numInput*=numInput;
+        }
+        else if (v.getId() == R.id.ExponentButton{
+            Log.i("Esther","SelectedExponent");
+            Math.exp();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
