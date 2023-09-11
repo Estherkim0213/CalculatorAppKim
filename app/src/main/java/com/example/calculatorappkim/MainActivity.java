258bda5169f252fb3c80621531fa0597e3a3d750
esther0213 <138899462+Estherkim0213@users.noreply.github.com>
2023-09-11 12:55:59 -0500

    tried to add calculator functions
    need to work on functions and why i have a white screen, maybe search stuff up...

--- app/src/main/java/com/example/calculatorappkim/MainActivity.java
+++ app/src/main/java/com/example/calculatorappkim/MainActivity.java
@@ -11,74 +11,111 @@ import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 
     TextView resultTv,solutionTv;
-    MaterialButton buttonC, buttonMultiply,buttonBrackOpen,buttonBrackClose
-    , buttonDivide, buttonAdd,buttonMinus,buttonExponent,buttonRoot,buttonEquals, buttonDecimal;
-    MaterialButton button0,button1,button2,button3,button4,button5,button6,
-            button7,button8,button9;
+//    MaterialButton buttonC, buttonMultiply,buttonBrackOpen,buttonBrackClose
+//    , buttonDivide, buttonAdd,buttonMinus,buttonExponent,buttonRoot,buttonEquals, buttonDecimal;
+//    MaterialButton button0,button1,button2,button3,button4,button5,button6,
+//            button7,button8,button9;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        resultTv = findViewById(R.id.OutputView);
+//        solutionTv = findViewById(R.id.inputView);
+//    }
+//
+//    void assignId(MaterialButton btn, int id){
+//        btn = findViewById(id);
+//        btn.setOnClickListener(this);
+//        numSelected();
+//    }
+
     private double numInput;
-    private ArrayList equation;
+    private String equation;
+    private double buttonSelected;
 
     public void numSelected(View v){
         if (v.getId() == R.id.button1){
             Log.i("Esther","Selected1");
-            equation.add(1);
+            buttonSelected = 1;
         }
         else if (v.getId() == R.id.button2){
             Log.i("Esther","Selected2");
-            equation.add(2);
+            buttonSelected = 2;
         }
         else if (v.getId() == R.id.button3){
             Log.i("Esther","Selected3");
-            equation.add(3);
+            buttonSelected = 3;
         }
         else if (v.getId() == R.id.button4){
             Log.i("Esther","Selected4");
-            equation.add(4);
+            buttonSelected = 4;
         }
         else if (v.getId() == R.id.button5){
             Log.i("Esther","Selected5");
-            equation.add(5);
+            buttonSelected = 5;
         }
         else if (v.getId() == R.id.button6){
             Log.i("Esther","Selected6");
-            equation.add(6);
+            buttonSelected = 6;
         }
         else if (v.getId() == R.id.button7){
             Log.i("Esther","Selected7");
-            equation.add(7);
+            buttonSelected = 7;
         }
         else if (v.getId() == R.id.button8){
             Log.i("Esther","Selected8");
-            equation.add(8);
+            buttonSelected = 8;
         }
         else if (v.getId() == R.id.button9){
             Log.i("Esther","Selected9");
-            equation.add(9);
+            buttonSelected = 9;
         }
+        else if (v.getId() == R.id.eButton){
+            Log.i("Esther","SelectedButtonE");
+            buttonSelected = 2.718281828459045;
+        }
+        else if (v.getId() == R.id.piButton){
+            Log.i("Esther","SelectedButtonPi");
+            buttonSelected =  3.141592653589793;
+        }
+        equation += buttonSelected;
+        solutionTv.setText(equation);
     }
     public void operatorSelected(View v){
         if (v.getId() == R.id.multiplyButton){
             Log.i("Esther","SelectedMultiply");
-            numInput*=numInput;
+            equation += "*";
         }
-        else if (v.getId() == R.id.ExponentButton{
+
+        else if (v.getId() == R.id.ExponentButton){
             Log.i("Esther","SelectedExponent");
-            Math.exp();
+            equation += "^";
+        }
+        else if (v.getId() == R.id.minusButton){
+            Log.i("Esther","SelectedMinus");
+            equation += "-";
+        }
+        else if (v.getId() == R.id.addButton){
+            Log.i("Esther","SelectedAdd");
+            equation += "+";
+        }
+        else if (v.getId() == R.id.RightParenthesesButton){
+            Log.i("Esther","SelectedRightBracket");
+            equation += ")";
+        }
+        else if (v.getId() == R.id.LeftParenthesesButton){
+            Log.i("Esther","SelectedLeftBracket");
+            equation += "(";
         }
 
+
     }
 
-    @Override
-    public void onClick(View v) {
-        
-    }
+
 }
